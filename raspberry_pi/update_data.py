7f2f0e3d92dd51151d2e9a86a62c4867d437481e
HaoruiPeng <haorui.peng@eit.lth.se>
2021-05-30 12:59:42 +0200

    simple script to send random coordinate data to the server

--- raspberry_pi/update_data.py
+++ raspberry_pi/update_data.py
@@ -1,13 +1,16 @@
 import requests
 import time
+import random
 
 SERVER_URL = "http://127.0.0.1:5000/clock"
-SerialNumber = '0000000'
+SerialNumber = '111111'
 params = {'SerialNumber': SerialNumber}
 
 with requests.Session() as session:
     while True:
-        current_time = {'time': time.time()}
-        resp = session.post(SERVER_URL, json=current_time, params=params)
+        current_location = {'x': round(random.random()*100,9),
+                            'y': round(random.random()*100,9)}
+        print(current_location)
+        resp = session.post(SERVER_URL, json=current_location, params=params)
         print(resp.text)
         time.sleep(2)
