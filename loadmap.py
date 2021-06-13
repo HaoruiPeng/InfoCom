51f19b42e47a00286b524f8e8a5bf3c3f6aca38d
HaoruiPeng <haorui.peng@eit.lth.se>
2021-06-13 17:49:00 +0200

    test

--- loadmap.py
+++ loadmap.py
@@ -18,17 +18,26 @@ graph = ox.graph_from_place(place_name, network_type='drive')
 nodes, edges = ox.graph_to_gdfs(graph)
 fig, ax = ox.plot_graph(graph, figsize=imsize/100, show=False)
 
-loc = "Kämnärsvägen 42, Skåne, Sweden"
+loc = "Ruben Rausings gata 11A, 223 55 Lund, Sweden"
 geolocator = Nominatim(user_agent="my_request")
 location = geolocator.geocode(loc)
 
-print(location.address)
+x, y = location.longitude, location.latitude
+
+print(location.longitude, location.latitude)
 # ax.plot(location.longitude, location.latitude, marker='o', markersize=10,
 #                     markerfacecolor="red", markeredgecolor="red")
-ax.plot(13.217381969757525, 55.72105809999999, marker='o', markersize=10,
+
+x_2, y_2 = 13.1968, 55.6882
+print(((x_2 - x)**2 + (y_2-y)**2)*10**6)
+
+ax.plot(x,y, marker='o', markersize=10,
+                    markerfacecolor="green", markeredgecolor="green")
+
+ax.plot(x_2, y_2, marker='o', markersize=10,
                     markerfacecolor="red", markeredgecolor="red")
-print(ax.get_xlim())
-print(ax.get_ylim())
+# print(ax.get_xlim())
+# print(ax.get_ylim())
 
 plt.show()
 # d="M 212.155699 768.96 L 968.644301 768.96 L 968.644301 103.68 L 212.155699 103.68 z "
